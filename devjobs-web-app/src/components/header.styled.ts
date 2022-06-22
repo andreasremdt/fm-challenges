bcd32722134f1037ce1bfd0d238b1dca655e7aa9
Andreas Remdt <me@andreasremdt.com>
2022-06-22 14:16:03 +0200

    fix: add vite base path

--- devjobs-web-app/src/components/header.styled.ts
+++ devjobs-web-app/src/components/header.styled.ts
@@ -7,15 +7,15 @@ export const Header = styled.header`
   margin-inline: auto;
   padding-inline: 40px;
   color: ${(props) => props.theme.colors.white};
-  background: transparent url(/desktop/bg-pattern-header.svg) no-repeat left top;
+  background: transparent url(desktop/bg-pattern-header.svg) no-repeat left top;
 
   @media (max-width: 768px) {
-    background-image: url(/tablet/bg-pattern-header.svg);
+    background-image: url(tablet/bg-pattern-header.svg);
   }
 
   @media (max-width: 600px) {
     padding-inline: 24px;
-    background-image: url(/mobile/bg-pattern-header.svg);
+    background-image: url(mobile/bg-pattern-header.svg);
     background-size: cover;
     height: 136px;
   }
