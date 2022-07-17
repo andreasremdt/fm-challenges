5cdecb75579cc6c9ad7ee9f92e12c9c8fcb99365
Andreas Remdt <me@andreasremdt.com>
2022-07-17 16:31:55 +0200

    feat: bootstrap markdown editor challenge

---
+++ in-browser-markdown-editor/src/app.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+
+function App() {
+  return (
+    <StrictMode>
+      <p>Hello World</p>
+    </StrictMode>
+  );
+}
+
+export default App;
---
+++ in-browser-markdown-editor/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import * as path from "path";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/devjobs-web-app/dist/",
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
