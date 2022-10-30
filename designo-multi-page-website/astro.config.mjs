35c77ffd247fc68369586e030d78bb6ebcf2123e
Andreas Remdt <me@andreasremdt.com>
2022-10-30 12:16:21 +0100

    feat: initial setup and first page on designo

---
+++ designo-multi-page-website/astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+export default defineConfig({
+  integrations: [tailwind({ config: { applyBaseStyles: false } })],
+});
