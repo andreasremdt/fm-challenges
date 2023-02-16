1a0e9937b008d9b3d262d90bd1fe13c4d982458f
Andreas Remdt <me@andreasremdt.com>
2023-02-16 14:52:26 +0100

    feat: add base apparel coming soon challenge

---
+++ base-apparel-coming-soon/README.md
@@ -0,0 +1,39 @@
+# Frontend Mentor - Base Apparel coming soon page solution
+
+![](./screenshot.png)
+
+This is a solution to the [Base Apparel coming soon page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/base-apparel-coming-soon-page-5d46b47f8db8a7063f9331a0). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [The challenge](#the-challenge)
+- [Links](#links)
+- [Built with](#built-with)
+- [Author](#author)
+
+## The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Receive an error message when the `form` is submitted if:
+  - The `input` field is empty
+  - The email address is not formatted correctly
+
+## Links
+
+- [Solution URL](https://github.com/andreasremdt/fm-challenges/tree/main/base-apparel-coming-soon/)
+- [Live Site URL](https://fm-challenges-ar.netlify.app/base-apparel-coming-soon/)
+
+## Built with
+
+- Semantic HTML5 markup
+- JavaScript
+- CSS custom properties
+- CSS Grid
+
+## Author
+
+- Website - [andreasremdt.com](https://andreasremdt.com)
+- Frontend Mentor - [@andreasremdt](https://www.frontendmentor.io/profile/andreasremdt)
---
+++ base-apparel-coming-soon/script.ts
@@ -0,0 +1,56 @@
+const form = document.querySelector("[data-form]") as HTMLFormElement;
+const input = document.querySelector("[data-validate]") as HTMLInputElement;
+
+function validate(element: HTMLInputElement): boolean {
+  if (element.checkValidity()) {
+    document.getElementById(`${element.name}-error`)?.remove();
+    element.removeAttribute("aria-invalid");
+    element.removeAttribute("aria-describedby");
+
+    return true;
+  }
+
+  if (element.hasAttribute("aria-invalid")) {
+    return false;
+  }
+
+  const error = document.createElement("p");
+
+  error.className = "error-text";
+  error.id = `${element.name}-error`;
+  error.textContent = "Please provide a valid email";
+  error.setAttribute("aria-live", "polite");
+
+  element.setAttribute("aria-invalid", "true");
+  element.setAttribute("aria-describedby", `${element.name}-error`);
+  element.insertAdjacentElement("afterend", error);
+
+  return false;
+}
+
+function handleSubmit(event: SubmitEvent) {
+  event.preventDefault();
+
+  if (validate(input)) {
+    const email = new FormData(event.target as HTMLFormElement).get("email");
+
+    alert(`Your email ${email} has been registered`);
+  }
+}
+
+function handleBlur(event: FocusEvent) {
+  validate(event.target as HTMLInputElement);
+}
+
+function handleInput(event: Event) {
+  const input = event.target as HTMLInputElement;
+
+  if (input.hasAttribute("aria-invalid")) {
+    validate(input as HTMLInputElement);
+  }
+}
+
+form.setAttribute("novalidate", "true");
+form.addEventListener("submit", handleSubmit);
+input.addEventListener("blur", handleBlur);
+input.addEventListener("input", handleInput);
