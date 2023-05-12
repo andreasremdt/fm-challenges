ade9eea4a12619f318db511e06b7dcdbe2ad2b43
Andreas Remdt <me@andreasremdt.com>
2023-05-12 12:08:01 +0200

    feat: add theme colors

--- product-feedback-app/tailwind.config.js
+++ product-feedback-app/tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    colors: {
+      primary: "#AD1FEA",
+      secondary: "#F49F85",
+      blue: {
+        DEFAULT: "#4661E6",
+        300: "#62BCFA",
       },
+      gray: {
+        600: "#373F68",
+        500: "#3A4374",
+        DEFAULT: "#647196",
+        300: "#F2F4FF",
+        200: "#F7F8FD",
+      },
+      white: "#FFFFFF",
     },
   },
   plugins: [],
-}
+};
