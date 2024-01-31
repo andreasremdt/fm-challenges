6a9792410997065f941483bb4d966174f36d2d96
Andreas Remdt <me@andreasremdt.com>
2024-01-31 16:30:49 +0100

    fix: remove unfinished challenges

--- in-browser-markdown-editor/js/main.js
+++
@@ -1,6 +0,0 @@
-import App from "./app.component.js";
-
-const app = new App();
-
-app.init();
-app.registerEvents();
--- in-browser-markdown-editor/js/utils.js
+++
@@ -1,7 +0,0 @@
-export function formatDate(date) {
-  return new Intl.DateTimeFormat("en-GB", {
-    month: "long",
-    day: "2-digit",
-    year: "numeric",
-  }).format(new Date(date));
-}
